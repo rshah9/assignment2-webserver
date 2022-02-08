ff9ca26f160bda6c3b600a3bb29b9d4a0740e36f
rshah9 <rgene2@gmail.com>
2022-02-08 18:14:51 -0500

    Webserver solution

--- solution.py
+++ solution.py
@@ -11,12 +11,12 @@ def webServer(port=13331):
   serverSocket.bind(("", port))
   #Fill in start
   serverSocket.listen(1)
-  print("The server is ready to receive")
+  #print("The server is ready to receive")
   #Fill in end
 
   while True:
     #Establish the connection
-    print('Ready to serve...')
+    #print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept()
     try:
 
