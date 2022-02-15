7170cef8b2f296905884faa31a93c2eeb97f635d
rshah9 <rgene2@gmail.com>
2022-02-15 12:59:59 -0500

    error code update 2

--- solution.py
+++ solution.py
@@ -43,7 +43,7 @@ def webServer(port=13331):
       except IOError:
         # Send response message for file not found (404)
         #Fill in start
-        connectionSocket.send("HTTP/1.0 400".encode())
+        connectionSocket.send("HTTP/1.0 400 404\r\n".encode()))
         #Fill in end
 
 
